6390d9b3e377ee8f0303258ad4b7b42cf61978f2
tmnlsthrn <me@tmnlsthrn.com>
2015-11-12 21:29:45 +0100

    [prefs] aboutlistcontroller

---
+++ prefs/HBCDAboutListController.h
@@ -0,0 +1,5 @@
+#import <CepheiPrefs/HBAboutListController.h>
+
+@interface HBCDAboutListController : HBAboutListController
+
+@end
---
+++ prefs/HBCDAboutListController.m
@@ -0,0 +1,10 @@
+#import "HBCDAboutListController.h"
+#import <UIKit/UITableViewCell+Private.h>
+
+@implementation HBCDAboutListController
+
++ (NSString *)hb_specifierPlist {
+	return @"About";
+}
+
+@end
